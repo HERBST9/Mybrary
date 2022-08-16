29dea6d83f0cd3485220a02bed8a4a14627466d9
Herja <anastasiazafeiropoyloy13@hotmail.com>
2022-08-16 13:14:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 const db = mongoose.connection
 db.on('error', error => console.log(error))
-db.once('open', () => console.log('Connected'))
+db.once('open', () => console.log('Connected')) 
 
 app.use('/', indexRouter)
 
